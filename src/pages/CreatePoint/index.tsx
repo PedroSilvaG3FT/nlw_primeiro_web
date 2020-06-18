a3d11af77f95d27ffa81b4b69e7f5575cc9f0522
Pedro Silva <pedro.silva-dev@hotmail.com>
2020-06-18 00:26:43 -0300

    Iniciando Mapa com leaflet

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../../assets/logo.svg";
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
 
 import "./CreatePoint.css";
 
@@ -50,18 +51,27 @@ const CreatePoint: React.FC = () => {
                         <span>Selecione o endereço no mapa</span>
                     </legend>
 
+                    <Map center={[-23.4856612, -46.3587532]} zoom={15}>
+                        <TileLayer
+                            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+
+                        <Marker position={[-23.4856612, -46.3587532]}/>
+                    </Map>
+
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">UF</label>
                             <select name="uf" id="uf">
-                              <option value="0">Selecione uma UF</option>
+                                <option value="0">Selecione uma UF</option>
                             </select>
                         </div>
 
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
                             <select name="city" id="city">
-                              <option value="0">Selecione uma cidade</option>
+                                <option value="0">Selecione uma cidade</option>
                             </select>
                         </div>
                     </div>
@@ -74,10 +84,13 @@ const CreatePoint: React.FC = () => {
                     </legend>
 
                     <ul className="items-grid">
-                      <li>
-                        <img src="http://localhost:3333/uploads/lampadas.svg" alt="Teste"/>
-                        <span>Lampadas</span>
-                      </li>
+                        <li>
+                            <img
+                                src="http://localhost:3333/uploads/lampadas.svg"
+                                alt="Teste"
+                            />
+                            <span>Lampadas</span>
+                        </li>
                     </ul>
                 </fieldset>
 
